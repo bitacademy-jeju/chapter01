aae886a79b9fcc56e677bd88fd18ab506e92fd66
user <user@user-PC>
2018-11-18 17:13:05 +0900

    반복문 테스트

---
+++ src/chapter01/ContinueTest02.java
@@ -0,0 +1,24 @@
+package chapter01;
+
+public class ContinueTest02 {
+
+	public static void main(String[] args) {
+//		int i = 0;
+//		while( i < 10 ) {
+//			if(i > 5) {
+//				continue;
+//			}
+//			System.out.println(i);
+//			i++;
+//		}
+		
+		for(int i = 0; i < 10; i++ ) {
+			if(i > 5) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+	}
+
+}
